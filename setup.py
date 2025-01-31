a9e00de4f3705cdcb9da6ace362b7e6945215072
Oli <hackerman@xsmail.com>
2025-01-31 09:23:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup_kwargs = {
             "nimpa/auxdata/*", "nimpa/acr_design/core_mumap/*", "nimpa/acr_design/core_nac/*",
             "nimpa/acr_design/rods/*", "nimpa/acr_design/sampling/*"]}, "install_requires": [
                 'dcm2niix', 'dipy>=1.3.0', 'imageio', 'miutil[nii]>=0.10.0', 'nibabel>=2.4.0',
-                'ninst @ git+https://github.com/OliJimbo/NInst/NInst.git@main', 'numpy>=1.14', 'pydicom>=1.0.2', 'scipy', 'setuptools', 'spm12',
+                'ninst @ git+https://github.com/OliJimbo/NInst.git@master', 'numpy>=1.14', 'pydicom>=1.0.2', 'scipy', 'setuptools', 'spm12',
                 'SimpleITK']}
 # 'SimpleITK>=1.2.0'
 cmake_args = [
